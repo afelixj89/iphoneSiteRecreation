5ed7257e69ce1ed1f47d24218019b036cfc51e5f
Antonio Felix <afelixj89@gmail.com>
2024-10-12 20:57:14 -0400

    iphone 3d models completed with orbital controls

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -4,6 +4,9 @@ import { useGSAP } from "@gsap/react";
 import ModelView from "./ModelView";
 import { yellowImg } from "../utils";
 import * as THREE from "three";
+import { View } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { models, sizes} from "../constants/index.js";
 
 const Model = () => {
   const [size, setSize] = useState("small");
@@ -42,7 +45,7 @@ const Model = () => {
               item={model}
               size={size}
             />
-             <ModelView
+            <ModelView
               index={2}
               groupRef={large}
               gsapType="view2"
@@ -51,6 +54,45 @@ const Model = () => {
               item={model}
               size={size}
             />
+            <Canvas
+              className="w-full h-full"
+              style={{
+                position: "fixed",
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                overflow: "hidden",
+              }}
+              eventSource={document.getElementById("root")}
+            >
+              <View.Port />
+            </Canvas>
+          </div>
+          <div className="mx-auto w-full">
+            <p className="text-sm font-light text-center mb-5">{model.title}</p>
+            <div className="flex-center ">
+              <ul className="color-container">
+                {models.map((item, i) => (
+                  <li
+                    key={i}
+                    className="w-6 h-5 rounded-full mx-2 cursor-pointer"
+                    style={{
+                      backgroundColor: item.color[0],
+                    }}
+                    onClick={() => setModel(item)}
+                  ></li>
+                ))}
+              </ul>
+              <button className="size-btn-container">
+                {sizes.map(({ label, value }) => (
+                  <span key={label} className="size-btn" style={{
+                    backgroundColor: size === value ? 'white' : 'transparent',
+                    color: size === value ? "black" : "white"
+                  }} onClick={() => setSize(value)}>{label}</span>
+                ))}
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import {
+  Html,
+  OrbitControls,
+  PerspectiveCamera,
+  View,
+} from "@react-three/drei";
+import Lights from "./Lights";
+import Iphone from "./Iphone";
+import * as THREE from "three";
 
-const ModelView = () => {
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationSize,
+  size,
+  item,
+}) => {
   return (
-    <div>
-      ModelView 
-    </div>
-  )
-}
+    <View
+      index={index}
+      id={gsapType}
+      className={`w-full h-full ${index === 2} ?  'right-[-100%]' : " " `}
+    >
+      <ambientLight intensity={0.3} />
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+      <Lights />
 
-export default ModelView
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      />
+      <group
+        ref={groupRef}
+        name={`${index === 1} ? 'small' : 'large'`}
+        position={[0, 0, 0]}
+      >
+        <Suspense
+          fallback={
+            <Html>
+              <div>Loading</div>
+            </Html>
+          }
+        >
+          <Iphone
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
+    </View>
+  );
+};
+
+export default ModelView;
